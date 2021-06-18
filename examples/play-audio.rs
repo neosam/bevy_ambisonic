df90ba2631b430dcc2914d2c00b3292d4076be75
Simon Goller <simongoller@Simons-Air.fritz.box>
2021-06-18 21:33:03 +0200

    User assets

--- examples/play-audio.rs
+++ examples/play-audio.rs
@@ -1,17 +1,20 @@
 use bevy::prelude::*;
 use bevy_ambisonic::Velocity;
 
-fn startup_system(mut commands: Commands) {
-    commands.spawn_bundle(bevy_ambisonic::AmbisonicBundle {
-        transform: Transform::from_xyz(-50.0, 0.0, 0.0),
-        source: bevy_ambisonic::AmbisonicSource::new_size(440, true),
-    }).insert(bevy_ambisonic::Velocity(Vec3::new(20.0, 0.0, 5.0)));
+fn startup_system(
+    mut commands: Commands,
+    mut sources: ResMut<Assets<bevy_ambisonic::AmbisonicSource>>,
+) {
+    let handle = sources.add(bevy_ambisonic::AmbisonicSource::new_size(440, true));
+    commands
+        .spawn_bundle(bevy_ambisonic::AmbisonicBundle {
+            transform: Transform::from_xyz(-50.0, 0.0, 0.0),
+            controller: bevy_ambisonic::AmbisonicController::new(handle),
+        })
+        .insert(bevy_ambisonic::Velocity(Vec3::new(20.0, 0.0, 5.0)));
 }
 
-fn update_system(
-    mut query: Query<(&mut Transform, &Velocity)>,
-    time: Res<Time>,
-) {
+fn update_system(mut query: Query<(&mut Transform, &Velocity)>, time: Res<Time>) {
     for (mut transform, bevy_ambisonic::Velocity(velocity)) in query.iter_mut() {
         transform.translation.x += velocity.x * time.delta_seconds();
         transform.translation.y += velocity.y * time.delta_seconds();
@@ -22,11 +25,8 @@ fn update_system(
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
-        .insert_non_send_resource(bevy_ambisonic::AmbisonicResource::default())
-        .add_startup_system(bevy_ambisonic::ambisonic_startup_system.system())
+        .add_plugin(bevy_ambisonic::AmbisonicPlugin)
         .add_startup_system(startup_system.system())
-        .add_system(bevy_ambisonic::ambisonic_sync_system.system())
-        .add_system(bevy_ambisonic::ambisonic_update_system.system())
         .add_system(update_system.system())
         .run();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,18 @@
+use std::sync::Arc;
+
 use ambisonic::rodio::source;
 use bevy::prelude::*;
+use bevy::reflect::TypeUuid;
 
-pub struct AmbisonicController {
-    controller: ambisonic::SoundController,
-}
-
-#[derive(Clone)]
+#[derive(Clone, TypeUuid)]
+#[uuid = "7cb323df-ef1e-47ef-b64a-d0996c631894"]
 pub struct AmbisonicSource {
-    data: Vec<f32>,
+    data: Arc<[f32]>,
     current_frame_len: Option<usize>,
-    channels: u16,
     sample_rate: u32,
     total_duration: Option<std::time::Duration>,
-    current_index: usize,
     repeat: bool,
+    current_index: usize,
 }
 impl ambisonic::rodio::Source for AmbisonicSource {
     fn current_frame_len(&self) -> Option<usize> {
@@ -21,7 +20,7 @@ impl ambisonic::rodio::Source for AmbisonicSource {
     }
 
     fn channels(&self) -> u16 {
-        self.channels
+        1
     }
 
     fn sample_rate(&self) -> u32 {
@@ -49,23 +48,37 @@ impl Iterator for AmbisonicSource {
     }
 }
 impl AmbisonicSource {
-    pub fn from_source<T: source::Source<Item=f32>>(source: T, repeat: bool) -> Self {
+    pub fn from_source<T: source::Source<Item = f32>>(source: T, repeat: bool) -> Self {
         let current_frame_len = source.current_frame_len();
-        let channels = source.channels();
         let sample_rate = source.sample_rate();
         let total_duration = source.total_duration();
         let current_index = 0;
-        let data = (0u16..u16::MAX).zip(source)
-            .map(|(_, x)| x)
-            .collect();
+        let data = (0u16..u16::MAX).zip(source).map(|(_, x)| x).collect();
         AmbisonicSource {
-            data, current_frame_len, channels, sample_rate, total_duration, current_index, repeat
+            data,
+            current_frame_len,
+            sample_rate,
+            total_duration,
+            repeat,
+            current_index,
         }
     }
 
     pub fn new_size(freq: u32, repeat: bool) -> Self {
         let sample = source::SineWave::new(freq);
         AmbisonicSource::from_source(sample, repeat)
+    }
+}
+pub struct AmbisonicController {
+    controller: Option<ambisonic::SoundController>,
+    handle: Handle<AmbisonicSource>,
+}
+impl AmbisonicController {
+    pub fn new(handle: Handle<AmbisonicSource>) -> Self {
+        AmbisonicController {
+            controller: None,
+            handle,
+        }
     }
 }
 
@@ -85,36 +98,36 @@ pub fn amibsonic_shutdown_system(mut ambisonic_res: NonSendMut<AmbisonicResource
     ambisonic_res.audio_scene = None;
 }
 
-pub fn ambisonic_sync_system(
-    mut query: Query<(Entity, &AmbisonicSource, &Transform, &Velocity)>,
-    scene: NonSend<AmbisonicResource>,
-    mut commands: Commands,
-) {
-    for (entity, source, transform, velocity) in query.iter_mut() {
-        dbg!("Sync");
-        commands.entity(entity).remove::<AmbisonicSource>();
-        let source: AmbisonicSource = source.clone();
-        if let Some(ref scene) = &scene.audio_scene {
-            let mut sound = AmbisonicController {
-                controller: scene.play_at(source, [transform.translation[0], transform.translation[1], transform.translation[2]]),
-            };
-            //if let Some(Velocity(velocity)) = velocity {
-                sound.controller.set_velocity([velocity.0[0], velocity.0[1], velocity.0[2]]);
-            //}
-            commands.entity(entity).insert(sound);
-        } else {
-            dbg!("Ambisonic not initialized");
-        }
-    }
-}
-
 pub fn ambisonic_update_system(
+    sources: Res<Assets<AmbisonicSource>>,
+    scene: NonSend<AmbisonicResource>,
     mut query: Query<(&Transform, &mut AmbisonicController, Option<&Velocity>)>,
 ) {
-    for (transform, mut source, velocity) in query.iter_mut() {
-        source.controller.adjust_position([transform.translation[0], transform.translation[1], transform.translation[2]]);
-        if let Some(Velocity(velocity)) = velocity {
-            source.controller.set_velocity([velocity[0], velocity[1], velocity[2]]);
+    for (transform, mut controller, velocity) in query.iter_mut() {
+        if let Some(ref mut controller) = controller.controller {
+            controller.adjust_position([
+                transform.translation[0],
+                transform.translation[1],
+                transform.translation[2],
+            ]);
+            if let Some(Velocity(velocity)) = velocity {
+                controller.set_velocity([velocity[0], velocity[1], velocity[2]]);
+            }
+        } else if let (Some(source), Some(ref scene)) =
+            (sources.get(controller.handle.clone()), &scene.audio_scene)
+        {
+            let mut sound_controller = scene.play_at(
+                source.clone(),
+                [
+                    transform.translation[0],
+                    transform.translation[1],
+                    transform.translation[2],
+                ],
+            );
+            if let Some(Velocity(velocity)) = velocity {
+                sound_controller.set_velocity([velocity[0], velocity[1], velocity[2]]);
+            }
+            controller.controller = Some(sound_controller);
         }
     }
 }
@@ -122,7 +135,17 @@ pub fn ambisonic_update_system(
 #[derive(Bundle)]
 pub struct AmbisonicBundle {
     pub transform: Transform,
-    pub source: AmbisonicSource,
+    pub controller: AmbisonicController,
+}
+
+pub struct AmbisonicPlugin;
+impl Plugin for AmbisonicPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_asset::<AmbisonicSource>()
+            .insert_non_send_resource(AmbisonicResource::default())
+            .add_startup_system(ambisonic_startup_system.system())
+            .add_system(ambisonic_update_system.system());
+    }
 }
 
 #[cfg(test)]
